8b75b823d257b34e77a41323bc9b23e9f1bc2205
S Naren Kumar <vssnaren@gmail.com>
2024-12-30 22:54:38 +0530

    normal change 0

--- portfolio-react/src/components/navbar.jsx
+++ portfolio-react/src/components/navbar.jsx
@@ -4,7 +4,7 @@ import finalImage from "../assets/final.png"; // Adjust the path based on the fi
 function Navbar() {
     return (
         <div className="navbar">
-            <img src={ffin dasaslImdaagse} height="50pxd" width="200" alt=s"erasr" />
+            <img src={ffin dasaslImdaagse} height="50pxd" width="200" alt=s"erasdr" />
         </div>
     );
 }
